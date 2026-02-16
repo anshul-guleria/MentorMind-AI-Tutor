ef0dd4bfc7cbd616bd0724eeba206735e1c1c1e5
Anshul Guleria <anshulguleria2002@gmail.com>
2026-02-16 11:09:19 +0530

    Changed local database to supabase

--- backend/database.py
+++ backend/database.py
@@ -8,13 +8,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
@@ -24,4 +26,3 @@ def get_db():
         db.close()
 
 
-Base = declarative_base()
